c4517d8b159ea531cfb4250bc946c88a8c2d79d4
Kyle Martin <kylecat98@gmail.com>
2017-09-06 01:34:55 -0400

    Initial Upload

---
+++ TranslatedProgram.cpp
@@ -0,0 +1,10 @@
+string prompt = "Enter password: ";
+cout << prompt;
+
+string password;
+cin >> password;
+
+if (password == "flag{0r4ng3_y0u_gl4d_1_d1dnt_s4y_b4n4n4s}")
+  cout << "Correct! \n";
+else
+  cout << "WRONG!";
